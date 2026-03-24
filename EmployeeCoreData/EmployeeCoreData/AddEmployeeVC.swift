c21983a97080cf2bea92653ca7dbfe8542ef1527
Tithi Shah <tithishah26@gmail.com>
2026-03-24 23:28:30 +0530

    Add AddEmployeeVC.swift (EmployeeCoreData)

---
+++ EmployeeCoreData/EmployeeCoreData/AddEmployeeVC.swift
@@ -0,0 +1,37 @@
+//
+//  AddEmployeeVC.swift
+//  EmployeeCoreData
+//
+//  Created by Tithi Shah on 3/24/26.
+//
+
+import UIKit
+
+class AddEmployeeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Customize back button for the pushed view controller
+//        let backButton = UIBarButtonItem()
+//        backButton.title = "Back"
+//        navigationItem.backBarButtonItem = backButton
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
