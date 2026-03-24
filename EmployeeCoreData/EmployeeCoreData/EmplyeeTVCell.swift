a1bbb4f260abc4cb3a5f96be605775d4b4be8519
Tithi Shah <tithishah26@gmail.com>
2026-03-24 23:43:02 +0530

    practice

---
+++ EmployeeCoreData/EmployeeCoreData/EmplyeeTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmplyeeTVCell.swift
+//  EmployeeCoreData
+//
+//  Created by Tithi Shah on 3/24/26.
+//
+
+import UIKit
+
+class EmplyeeTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
