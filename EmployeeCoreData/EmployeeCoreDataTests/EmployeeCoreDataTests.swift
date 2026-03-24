f8e81fd41a93c9779f5868e787bd915ecfbda9c8
Tithi Shah <tithishah26@gmail.com>
2026-03-24 23:39:16 +0530

    Add EmployeeCoreDataTests.swift

---
+++ EmployeeCoreData/EmployeeCoreDataTests/EmployeeCoreDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeCoreDataTests.swift
+//  EmployeeCoreDataTests
+//
+//  Created by Tithi Shah on 3/24/26.
+//
+
+import Testing
+@testable import EmployeeCoreData
+
+struct EmployeeCoreDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
