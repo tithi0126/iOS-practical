e967298ad4e10a12d851dac8f3a232d2c73c2c80
Tithi Shah <tithishah26@gmail.com>
2026-03-24 23:26:48 +0530

    Add studentCoreData2Tests.swift

---
+++ studentCoreData2/studentCoreData2Tests/studentCoreData2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  studentCoreData2Tests.swift
+//  studentCoreData2Tests
+//
+//  Created by Tithi Shah on 3/24/26.
+//
+
+import Testing
+@testable import studentCoreData2
+
+struct studentCoreData2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
